3a15ef8136426c379e14bb453b1a487a90d8bfb8
hugosenari <hugosenari@gmail.com>
2022-03-12 00:36:35 -0300

    chore: re index asynctools

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Various asynchronous tools for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-asynctools-master.flake = false;
+  inputs.src-asynctools-master.owner = "cheatfate";
+  inputs.src-asynctools-master.ref   = "refs/heads/master";
+  inputs.src-asynctools-master.repo  = "asynctools";
+  inputs.src-asynctools-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-asynctools-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-asynctools-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
